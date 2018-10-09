776b129ec717500cb3b2c817b1b67bd764a4e390
Omar Sanchez <Osanchez@umass.edu>
2018-10-08 23:05:40 -0400

    minimax attempt

--- src/Connect4Board.java
+++ src/Connect4Board.java
@@ -4,16 +4,14 @@ public class Connect4Board {
     private static int rows = 4;
     private static int columns = 5;
     private String[][] board = new String[columns][rows];
+
     private Connect4Board parentBoard = null;
     public ArrayList<Connect4Board> children = new ArrayList<>();
+
     public String playerTurn;
-    public boolean isTerminal;
-    public int min;
-    public int max;
+    public boolean isTerminal = false;
+    public int utility;
 
-    public Connect4Board() {
-
-    }
 
     public void setBoard(String[][] board) {
         this.board = board;
@@ -23,24 +21,11 @@ public class Connect4Board {
         this.parentBoard = parent;
     }
 
-
-    public void addChild(Connect4Board child) {
-        this.children.add(child);
-    }
-
     public void setNextPlayerTurn() {
         if(this.playerTurn.equals("X")) {
             this.playerTurn = "O";
         } else {
             this.playerTurn = "X";
-        }
-    }
-
-    public void copyBoard(String[][] original, String[][] copy) {
-        for(int x = 0; x < rows; x++) {
-            for(int y = 0; y < columns; y++) {
-                copy[x][y] = original[x][y];
-            }
         }
     }
 
@@ -76,6 +61,18 @@ public class Connect4Board {
         }
     }
 
+    public void addChild(Connect4Board child) {
+        this.children.add(child);
+    }
+
+    public void copyBoard(String[][] original, String[][] copy) {
+        for(int x = 0; x < rows; x++) {
+            for(int y = 0; y < columns; y++) {
+                copy[x][y] = original[x][y];
+            }
+        }
+    }
+
     public void printBoard() {
         for (int x = 0; x < rows; x++) {
             for (int y = 0; y < columns; y++) {
@@ -89,125 +86,99 @@ public class Connect4Board {
         }
     }
 
-        //is terminal
+
+    //Utility Function
+    public int getUtility() {
+        return this.utility;
+    }
+
     public boolean isTerminal() {
-        if(isHorizotalWin() | isVerticleWin() | isDiagonalWin()| isDraw()) {
-            return true;
-        } else {
-            return false;
-        }
+        isGoal();
+        return this.isTerminal;
     }
 
-    private boolean isDraw() {
-        int filledBoard = columns * rows;
-        int spacesOccupied;
+    private int isGoal() {
+        //Horizontal Check
         int countX = 0;
-        int countY = 0;
-
-        for(int x = 0; x < rows - 1; x++) {
-            for(int y = 0; y < columns - 1; y++) {
-
-                if(this.board[x][y].equals("X")) {
-                    countX++;
-                }
-                if(this.board[x][y].equals("O")) {
-                    countY++;
-                }
-            }
-        }
-
-        spacesOccupied = countX + countY;
-
-        if(spacesOccupied == filledBoard) {
-            System.out.println("Player X and O Draw");
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-
-    private boolean isVerticleWin() {
-        int Xcount = 0;
-        int Ocount = 0;
-
-        for (int x = 0; x <= columns - 1; x++) {
-            Xcount = 0;
-            Ocount = 0;
-            for (int y = rows - 1; y >= 0; y--) {
-                //if space is occupied by current player's game piece, increment counter
-                if (this.board[y][x].equals("X")) {
-                    Xcount++;
-                    Ocount = 0;
-                }
-                if (this.board[y][x].equals("O")) {
-                    Ocount++;
-                    Xcount = 0;
-                }
-                if (this.board[y][x].equals(".")) {
-                    Xcount = 0;
-                    Ocount = 0;
-                }
-                if (Xcount == 3) { //3 of given kind found, return player winner
-                    System.out.println("Player X Wins Vertically!");
-                    return true;
-                }
-                if (Ocount == 3) { //3 of given kind found, return player winner
-                    System.out.println("Player O Wins Vertically!");
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    private boolean isHorizotalWin() {
-        int Xcount = 0;
-        int Ocount = 0;
+        int countO = 0;
 
         for (int x = rows - 1; x >= 0; x--) {
-            Xcount = 0;
-            Ocount = 0;
+            countX = 0;
+            countO = 0;
             for (int y = 0; y <= columns - 1; y++) {
                 //if space is occupied by current player's game piece, increment counter
                 if (this.board[x][y].equals("X")) {
-                    Xcount++;
-                    Ocount = 0;
+                    countX++;
+                    countO = 0;
                 }
                 if (this.board[x][y].equals("O")) {
-                    Ocount++;
-                    Xcount = 0;
+                    countO++;
+                    countX = 0;
                 }
                 if (this.board[x][y].equals(".")) {
-                    Xcount = 0;
-                    Ocount = 0;
+                    countX = 0;
+                    countO = 0;
                 }
-                if (Xcount == 3) { //3 of given kind found, return player winner
-                    System.out.println("Player X Wins Horizontally!");
-                    return true;
+                if (countX == 3) { //3 of given kind found, return player winner
+                    //System.out.println("Player X Wins Horizontally!");
+                    this.isTerminal = true;
+                    this.utility = 1;
+                    return 1;
                 }
-                if (Ocount == 3) { //3 of given kind found, return player winner
-                    System.out.println("Player O Wins Horizontally!");
-                    return true;
+                if (countO == 3) { //3 of given kind found, return player winner
+                    //System.out.println("Player O Wins Horizontally!");
+                    this.isTerminal = true;
+                    this.utility = -1;
+                    return 1;
                 }
             }
         }
-        return false;
-    }
 
-    //checks if a player has won diagonally
-    private boolean isDiagonalWin() {
-        // ascendingDiagonalCheck
-        //sides
-        int countX = 0;
-        int countO = 0;
+        //vertical Check
+        countX = 0;
+        countO = 0;
+        for (int x = 0; x <= columns - 1; x++) {
+            countX = 0;
+            countO = 0;
+            for (int y = rows - 1; y >= 0; y--) {
+                //if space is occupied by current player's game piece, increment counter
+                if (this.board[y][x].equals("X")) {
+                    countX++;
+                    countO = 0;
+                }
+                if (this.board[y][x].equals("O")) {
+                    countO++;
+                    countX = 0;
+                }
+                if (this.board[y][x].equals(".")) {
+                    countX = 0;
+                    countO = 0;
+                }
+                if (countX == 3) { //3 of given kind found, return player winner
+                    //System.out.println("Player X Wins Vertically!");
+                    this.isTerminal = true;
+                    this.utility = 1;
+                    return 1;
+                }
+                if (countO == 3) { //3 of given kind found, return player winner
+                    //System.out.println("Player O Wins Vertically!");
+                    this.isTerminal = true;
+                    this.utility = -1;
+                    return 1;
+                }
+            }
+        }
+
+        //Diagonal Check
+        //sides - ascendingDiagonalCheck
         int jSides = 0;
+        countX = 0;
+        countO = 0;
 
         for (int i = 2; i < 4; i++) { //this checks all 3 diagonal squares even though after one missing piece its impossible to win
             countX = 0;
             countO = 0;
             for (int k = 0; k < 3; k++) {
-                //System.out.println(i + " - " + k + ", " + jSides + " + " + k + " = " + (i - k) + ", " + (jSides + k));
                 if (this.board[i - k][jSides + k].equals("X")) {
                     countX++;
                     countO = 0;
@@ -221,26 +192,28 @@ public class Connect4Board {
                     countO = 0;
                 }
                 if (countX == 3) { //3 of given kind found, return player winner
-                    System.out.println("Player X Wins Diagonally Ascending Side!");
-                    return true;
+                    //System.out.println("Player X Wins Diagonally Ascending Side!");
+                    this.isTerminal = true;
+                    this.utility = 1;
+                    return 1;
                 }
                 if (countO == 3) { //3 of given kind found, return player winner
-                    System.out.println("Player O Wins Diagonally Ascending Side!");
-                    return true;
+                    //System.out.println("Player O Wins Diagonally Ascending Side!");
+                    this.isTerminal = true;
+                    this.utility = -1;
+                    return 1;
                 }
             }
             jSides += 2;
         }
 
         //Center
-        countX = 0; //reset counters
+        countX = 0;
         countO = 0;
-
         for (int i = 0; i <= 1; i++) {
             countX = 0;
             countO = 0;
             for (int k = 0; k < 4; k++) {
-                //System.out.println(3 + " - " + k + ", " + i + " + " + k + " = " + (3 - k) + ", " + (i+k));
                 if (this.board[3 - k][i + k].equals("X")) {
                     countX++;
                     countO = 0;
@@ -254,27 +227,29 @@ public class Connect4Board {
                     countO = 0;
                 }
                 if (countX == 3) { //3 of given kind found, return player winner
-                    System.out.println("Player X Wins Diagonally Ascending Center!");
-                    return true;
+                    //System.out.println("Player X Wins Diagonally Ascending Center!");
+                    this.isTerminal = true;
+                    this.utility = 1;
+                    return 1;
                 }
                 if (countO == 3) { //3 of given kind found, return player winner
-                    System.out.println("Player O Wins Diagonally Ascending Center!");
-                    return true;
+                    //System.out.println("Player O Wins Diagonally Ascending Center!");
+                    this.isTerminal = true;
+                    this.utility = -1;
+                    return 1;
                 }
             }
         }
 
         // descendingDiagonalCheck
         //sides
-        countX = 0; //reset counters
-        countO = 0;
-
         jSides = 2;
+        countX = 0;
+        countO = 0;
         for (int i = 3; i > 1; i--) { //this checks all 3 diagonal squares even though after one missing piece its impossible to win
             countX = 0;
             countO = 0;
             for (int k = 0; k < 3; k++) {
-                //System.out.println(i + " - " + k + ", " + jSides + " + " + k + " = " + (i - k) + ", " + (jSides - k));
                 if (this.board[i - k][jSides - k].equals("X")) {
                     countX++;
                     countO = 0;
@@ -288,50 +263,80 @@ public class Connect4Board {
                     countO = 0;
                 }
                 if (countX == 3) { //3 of given kind found, return player winner
-                    System.out.println("Player X Wins Diagonally Descending Side!");
-                    return true;
+                    //System.out.println("Player X Wins Diagonally Descending Side!");
+                    this.isTerminal = true;
+                    this.utility = 1;
+                    return 1;
                 }
                 if (countO == 3) { //3 of given kind found, return player winner
-                    System.out.println("Player O Wins Diagonally Descending Side!");
-                    return true;
+                    //System.out.println("Player O Wins Diagonally Descending Side!");
+                    this.isTerminal = true;
+                    this.utility = -1;
+                    return 1;
                 }
             }
             jSides += 2;
         }
 
         // Center
-         jSides = 3;
-        countX = 0; //reset counters
+        jSides = 3;
+        countX = 0;
         countO = 0;
-
         for (int i = 4; i > 2; i--) {
             countX = 0;
             countO = 0;
-                for (int k = 0; k < 4; k++) {
-                    //System.out.println(jSides + " - " + k + ", " + i + " - " + k + " = " + (jSides - k) + ", " + (i - k));
-                    if (this.board[jSides - k][i - k].equals("X")) {
-                        countX++;
-                        countO = 0;
-                    }
-                    if (this.board[jSides - k][i - k].equals("O")) {
-                        countO++;
-                        countX = 0;
-                    }
-                    if (this.board[jSides - k][i - k].equals(".")) {
-                        countX = 0;
-                        countO = 0;
-                    }
-                    if (countX == 3) { //3 of given kind found, return player winner
-                        System.out.println("Player X Wins Diagonally Descending Center!");
-                        return true;
-                    }
-                    if (countO == 3) { //3 of given kind found, return player winner
-                        System.out.println("Player O Wins Diagonally Descending Center!");
-                        return true;
-                    }
+            for (int k = 0; k < 4; k++) {
+                if (this.board[jSides - k][i - k].equals("X")) {
+                    countX++;
+                    countO = 0;
+                }
+                if (this.board[jSides - k][i - k].equals("O")) {
+                    countO++;
+                    countX = 0;
+                }
+                if (this.board[jSides - k][i - k].equals(".")) {
+                    countX = 0;
+                    countO = 0;
+                }
+                if (countX == 3) { //3 of given kind found, return player winner
+                    //System.out.println("Player X Wins Diagonally Descending Center!");
+                    this.isTerminal = true;
+                    this.utility = 1;
+                    return 1;
+                }
+                if (countO == 3) { //3 of given kind found, return player winner
+                    //System.out.println("Player O Wins Diagonally Descending Center!");
+                    this.isTerminal = true;
+                    this.utility = -1;
+                    return 1;
+                }
             }
         }
-        return false;
+
+        //Draw Check
+        int filledBoard = columns * rows;
+        countX = 0;
+        countO = 0;
+
+        for(int x = 0; x < rows - 1; x++) {
+            for(int y = 0; y < columns - 1; y++) {
+
+                if(this.board[x][y].equals("X")) {
+                    countX++;
+                }
+                if(this.board[x][y].equals("O")) {
+                    countO++;
+                }
+            }
+        }
+
+        if((countX + countO) == filledBoard) {
+            //System.out.println("Player X and O Draw");
+            this.isTerminal = true;
+            this.utility = 0;
+            return 1;
+        }
+        return 0;
     }
 
     //Board move
@@ -343,7 +348,6 @@ public class Connect4Board {
         childBoard.playerTurn = this.playerTurn;
         childBoard.setParent(this);
         childBoard.setNextPlayerTurn();
-
 
         //make board move
         for (int x = 3; x >= 0; x--) {
--- src/MiniMax.java
+++ src/MiniMax.java
@@ -1,39 +1,54 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class MiniMax {
 
+    public Connect4Board miniMaxDecision(Connect4Board state) {
+       return state;
+    }
+
+    public int maxValue(Connect4Board state) {
+        if(state.isTerminal()) {
+            return state.getUtility();
+        }
+
+        int v = Integer.MIN_VALUE;
+
+        state.expand();
+
+        for(Connect4Board a: state.children) {
+            v = Math.max(v, minValue(a));
+        }
+        return v;
+    }
+
+    public int minValue(Connect4Board state) {
+        if(state.isTerminal()) {
+            return state.getUtility();
+        }
+
+        int v = Integer.MAX_VALUE;
+
+        state.expand();
+
+        for(Connect4Board a: state.children) {
+            v = Math.min(v, maxValue(a));
+        }
+        return v;
+    }
+
     public static void main(String[] args) {
-        Connect4Board initialBoard = new Connect4Board();
         IOHandler reader = new IOHandler();
         reader.readFile("Files/board.txt");
         String[][] readBoard = reader.boardState;
 
+        Connect4Board initialBoard = new Connect4Board();
         initialBoard.setBoard(readBoard);
-        initialBoard.printBoard();
         initialBoard.getCurrentPlayerTurn();
-        System.out.println();
 
-        //minimax algorithm
-        LinkedList<Connect4Board> boardStates = new LinkedList<>();
-        LinkedList<Connect4Board> leafNodes = new LinkedList<>();
-        boardStates.add(initialBoard);
+        MiniMax miniMax = new MiniMax();
 
-        while(!boardStates.isEmpty()) {
-            Connect4Board current = boardStates.remove();
-            if(!current.isTerminal()) {
-                current.expand();
-                for (Connect4Board child : current.children) {
-                    boardStates.add(child);
-                }
-            } else {
-                leafNodes.add(current);
-            }
-        }
-        System.out.println("Winning Boards");
-        for(int x = 0; x < 22767; x++) {
-            System.out.println();
-            leafNodes.get(x).printBoard();
-            leafNodes.get(x).isTerminal();
-        }
+        System.out.println(miniMax.maxValue(initialBoard));
+        System.out.println(miniMax.minValue(initialBoard));
     }
 }
